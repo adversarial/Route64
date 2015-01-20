438d8d50c82d3a4c15f1e6d6bfe32a885df66a9a
vividrev <netrev@outlook.com>
2015-01-20 13:34:34 -0800

    Updated API

--- Route64.h
+++ Route64.h
@@ -1,18 +1,33 @@
 #pragma once
 
+/*
+    Route64 API
+        by ~vividrev
+        
+    Implements several routines to "break  through" the WoW64 layer.
+ */
 #include <stdint.h>
 
 #ifndef _M_IX86
 #error "Only x86-32 is valid for Route64 library"
 #endif
 
-typedef void* (__stdcall *stdcall_f)();
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-void* __stdcall WoW64Stdcallx64(stdcall_f AddrLow, stdcall_f AddrHigh,
-                                    unsigned int args_cb, void** args);
+// Calls a function in x64 mode with stdcall convention using rcx, rdx, r8, r9, ...
+// Returns false if not in WoW64 mode (x64 not available)
+bool __stdcall WoW64Stdcallx64(uintptr_t CalleeHigh, uintptr_t CalleeLow, 
+                               uintptr_t* RetValHigh, uintptr_t* RetValLow, 
+                               uint args_cb, uint64_t* args);
 
-uint32_t __stdcall WoW64Write(void* DestLow, void* DestHigh,
-                              const void* Src, size_t cbSrc);
-
-uint32_t __stdcall WoW64Read(const void* SrcLow, const void* SrcHigh,
-                             void* Dest, size_t cbDest);
+// Copies memory to/from 64 bit addresses
+// Returns bytes copied
+size_t __stdcall WoW64Write(uintptr_t DestHigh, uintptr_t DestLow, 
+                            uintptr_t SrcHigh, uintptr_t SrcLow, 
+                            size_t cbHigh, size_t cbLow);
+                              
+#ifdef __cplusplus
+}
+#endif
